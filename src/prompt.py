4efd296ad44b6940f1b5de6188124f223e845819
Shravani-Saratkar <164533558+Shravani-Saratkar@users.noreply.github.com>
2025-03-20 21:06:27 +0530

    The chatbot is ready

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "You are an assistant for question-answering tasks."
+    "Use the following pieces of retrieved context to answer"
+    "the questions. If you don't know the answer ,say that you"
+    "don't have enough information. Use five sentences  and keep the"
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
